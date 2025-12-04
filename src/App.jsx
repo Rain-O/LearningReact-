d2b1f78e2700a840faff0938244184b536d094de
Rian Freitas <Rian Freitas>
2025-12-04 09:50:06 -0300

    Add mx-auto in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -56,8 +56,8 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px] space-y-6">
+    <div className="w-screen h-screen bg-slate-500 mx-auto flex justify-center p-6">
+      <div className="w-[500px] mx-auto space-y-6">
         <Title>Gerenciador de Tarefas</Title>
         <AddTask AddTaskClick={AddTaskClick} />
         <Tasks
